00126dfb5429b9661b9ba67fe8b7b4c0ea5da298
DIMESH <DIMESH@DESKTOP-2AKPA9H>
2019-12-26 01:57:30 +0530

    added the login

--- src/interfaces/Login.java
+++ src/interfaces/Login.java
@@ -21,75 +21,37 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
 
-        jPanel2.setBackground(new java.awt.Color(102, 6, 247));
-        jPanel2.setPreferredSize(new java.awt.Dimension(1050, 600));
-        jPanel2.setLayout(null);
+        jLabel4.setText("jLabel4");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(390, 180, 34, 14);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("User Name:");
-        jPanel2.add(jLabel1);
-        jLabel1.setBounds(28, 116, 94, 31);
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Password:");
-        jPanel2.add(jLabel2);
-        jLabel2.setBounds(28, 198, 86, 34);
-        jPanel2.add(jPasswordField1);
-        jPasswordField1.setBounds(177, 199, 195, 34);
-        jPanel2.add(jTextField1);
-        jTextField1.setBounds(177, 117, 195, 31);
-
-        jButton1.setBackground(new java.awt.Color(204, 0, 0));
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
-        jButton1.setText("Cancel");
-        jPanel2.add(jButton1);
-        jButton1.setBounds(78, 371, 92, 37);
-
-        jButton3.setBackground(new java.awt.Color(0, 204, 204));
-        jButton3.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
-        jButton3.setText("Log in");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-        jPanel2.add(jButton3);
-        jButton3.setBounds(270, 371, 84, 37);
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 22)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setText("Login");
-        jPanel2.add(jLabel3);
-        jLabel3.setBounds(28, 13, 143, 41);
-
-        getContentPane().add(jPanel2);
-        jPanel2.setBounds(270, 20, 410, 550);
+        jLabel2.setIcon(new javax.swing.ImageIcon("E:\\2nd year\\JAVA Projects\\BMS\\src\\Images\\login5.png")); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(260, 40, 448, 540);
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/blur2.png"))); // NOI18N
         getContentPane().add(jLabel5);
-        jLabel5.setBounds(-60, -20, 1050, 630);
+        jLabel5.setBounds(0, 0, 1050, 630);
+
+        jLabel6.setText("jLabel6");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(530, 480, 110, 40);
+
+        jButton1.setText("jButton1");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(530, 480, 120, 50);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
 
   
     public static void main(String args[]) {
@@ -103,13 +65,9 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel jLabel6;
     // End of variables declaration//GEN-END:variables
 }
